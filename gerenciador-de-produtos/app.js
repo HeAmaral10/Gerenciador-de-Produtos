6c58f152ab539f33eafb5828407d30646a6ee21f
HeAmaral10 <hefacebook100@gmail.com>
2025-03-10 17:38:41 -0300

    Complementação no App

--- gerenciador-de-produtos/app.js
+++ gerenciador-de-produtos/app.js
@@ -4,10 +4,14 @@ import produto from "./produtos.js";
 // Chamada das funções
 console.log("SISTEMA DE GERENCIAMENTO DE PRODUTOS");
 console.log(produto.gerarLinha(80));
+console.log("Lista de Todos os Produtos");
 console.log(produto.allProducts());
 console.log(produto.gerarLinha(80));
+console.log("Produtos Acima de R$100");
 console.log(`${produto.valueProduct()}`);
 console.log(produto.gerarLinha(80));
+console.log("Quantidade Total de Produtos");
 console.log(produto.totalProducts());
 console.log(produto.gerarLinha(80));
+console.log("Produtos de Fornecedor Especifíco");
 console.log(`${produto.fornecedorProduct("Oakley")}`);
